36ceb10d2c3ede5d9753cfe213dda96ca2dbd0b6
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-06-11 16:12:26 -0600

    fixed forward slash typo for server request messag

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
  
 // .env for our security
 
-require('dotenv').config()
+require('dotenv').config();
 
 // port options
 
@@ -13,9 +13,9 @@ const port = process.env.PORT || 5000;
 
 //lets send a message to our port 
 
-app.get('/'), (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello Node World !'); 
-}
+});
 
 
 // time to connect to our port so that it listens to our code
