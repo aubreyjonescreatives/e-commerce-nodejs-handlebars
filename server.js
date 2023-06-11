49eb6ac43c58b5f41e3ccda6f73343736556ab4a
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-06-11 15:54:59 -0600

    connected to port and added comments throughout js

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
+// added packages
+
 const express = require('express'); 
 const app = express(); 
  
-
+// .env for our security
 
 require('dotenv').config()
 
-const port = process.env.PORT;
+// port options
+
+const port = process.env.PORT || 5000;
+
+//lets send a message to our port 
 
 app.get('/'), (req, res) => {
-    res.sent('Hello Node World !'); 
+    res.send('Hello Node World !'); 
 }
+
+
+// time to connect to our port so that it listens to our code
+
+
+app.listen(port, () => {
+    console.log(`Server now listening on port ${port} `);
+});
